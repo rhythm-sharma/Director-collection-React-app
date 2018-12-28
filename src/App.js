43e653c4de63b9c32f3f0fa6b0a3873d7958c9ac
Rhythm <rrhythmsharma@gmail.com>
2018-12-28 23:53:42 +0530

    finish with styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import {directors} from './director';
 import SearchBox from './SearchBox'
+import './App.css';
 
-const App = () => {
-    return(
-        <div className='tc'> 
-        <h1>Directors</h1>
-        <SearchBox />
-        <CardList directors={directors}/>
-        </div>
-    );
+
+class App  extends Component {
+    constructor(){
+        super()
+        this.state = {
+            directors: directors,
+            searchfield: ''
+        }
+    }
+
+    onsearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render(){ 
+        const filterdirectors = this.state.directors.filter(directors => {
+            return directors.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return(
+            <div className='tc'> 
+                <h1 className="f1">Directors</h1>
+                <SearchBox searchChange = {this.onsearchChange}/>
+                <CardList directors={filterdirectors}/>
+            </div>
+        );
+    }
 }
+
 
 export default App;
