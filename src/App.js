8d21ea15263fee6aa9fb483f2a259d34c21ea722
Rhythm <rrhythmsharma@gmail.com>
2018-12-28 22:56:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import CardList from './CardList';
+import {directors} from './director';
+import SearchBox from './SearchBox'
 
 const App = () => {
-    return();
+    return(
+        <div className='tc'> 
+        <h1>Directors</h1>
+        <SearchBox />
+        <CardList directors={directors}/>
+        </div>
+    );
 }
+
+export default App;
--- src/CardList.js
+++ src/CardList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({directors}) => {
-    const cardArray = directors.map((user, i) => {
-         return(<Card id={directors[i].id} name={directors[i].name} birthname={directors[i].birthname} birthdate={directors[i].birthdate} birthplace={directors[i].birthplace}/>)
-    })
+const CardList = ({directors}) => { 
     return(
         <div className='tc'>
-         {cardArray} 
+         {
+            directors.map((user, i) => {
+                return(
+                <Card key={i} 
+                    id={directors[i].id}
+                    name={directors[i].name}
+                    birthname={directors[i].birthname} 
+                    birthdate={directors[i].birthdate} 
+                    birthplace={directors[i].birthplace}
+                />)
+            })
+         } 
         </div>
     );
 }
