207c37568d2c55e46cddf0b3198f430397f3c6e8
Rhythm <rrhythmsharma@gmail.com>
2018-12-29 16:21:34 +0530

    add scroll, style and use a  rest API and make file structure cleaner

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import {directors} from './director';
-import SearchBox from './SearchBox'
+import CardList from '../components/CardList';
+import {directors} from '../director';
+import SearchBox from '../components/SearchBox'
 import './App.css';
-import Scroll from './Scroll'
+import Scroll from '../components/Scroll'
 
 
 class App  extends Component {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import CardList from './CardList';
+import App from './containers/App';
+import CardList from './components/CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import {directors} from './director';
